1ee5b0797e6f26d471fda4a150e88f914aeac1af
saurabh <vksaha99340@gmail.com>
2022-08-09 19:01:10 +0530

    NavBar with transition effect

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAgnUC6_EuykAWu7S5DgcmfLog8ZMe5gx4",
+  authDomain: "netflix-clone-44605.firebaseapp.com",
+  projectId: "netflix-clone-44605",
+  storageBucket: "netflix-clone-44605.appspot.com",
+  messagingSenderId: "645081970515",
+  appId: "1:645081970515:web:c5f56cbedafcd4e15cd3a6",
+};
