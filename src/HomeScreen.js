993c04bf4366a47a05dffe9a89907e1cb4b65c9d
saurabh <vksaha99340@gmail.com>
2022-08-17 22:42:36 +0530

    Movie Rows Implementation

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./HomeScreen.css";
 import Nav from "./Nav";
 import Banner from "./Banner";
+import Row from "./Row";
+import requests from "./Requests";
 
 function HomeScreen() {
   return (
@@ -11,7 +13,35 @@ function HomeScreen() {
 
       {/*Banner */}
       <Banner />
+
       {/*Row */}
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} isLargeRow />
+
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} isLargeRow />
+
+      <Row
+        title="Action Movies"
+        fetchUrl={requests.fetchActionMovies}
+        isLargeRow
+      />
+
+      <Row
+        title="Comedy Movies"
+        fetchUrl={requests.fetchComedyMovies}
+        isLargeRow
+      />
+
+      <Row
+        title="Horror Movies"
+        fetchUrl={requests.fetchHorrorMovies}
+        isLargeRow
+      />
     </div>
   );
 }
