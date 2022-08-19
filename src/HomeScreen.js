7306fcec764d7a74bb47828ccaf7d14a8d4c7f89
saurabh <vksaha99340@gmail.com>
2022-08-19 17:47:34 +0530

    Final Implementation of Rows

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -21,27 +21,15 @@ function HomeScreen() {
         isLargeRow
       />
 
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} isLargeRow />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
 
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} isLargeRow />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
 
-      <Row
-        title="Action Movies"
-        fetchUrl={requests.fetchActionMovies}
-        isLargeRow
-      />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
 
-      <Row
-        title="Comedy Movies"
-        fetchUrl={requests.fetchComedyMovies}
-        isLargeRow
-      />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
 
-      <Row
-        title="Horror Movies"
-        fetchUrl={requests.fetchHorrorMovies}
-        isLargeRow
-      />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
     </div>
   );
 }
